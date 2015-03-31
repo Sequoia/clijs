6e245119cf9d4269e73427f59e44ec18c1244f71
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-03-30 22:55:35 -0400

    remove lesson menu from package.json close #12

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
-var path = require('path')
-var adventureMap = require('adventure-map')
-var pkg = require('./package.json')
+var path = require('path');
+var adventureMap = require('adventure-map');
+var pkg = require('./package.json');
+var exercises = require('./exercises/menu.json');
 
 // resolve all package.json exercises relative
 // to this directory.
-Object.keys(pkg.exercises).forEach(function(name) {
-  pkg.exercises[name] = path.resolve(__dirname, pkg.exercises[name])
-})
+Object.keys(exercises).forEach(function(name) {
+  exercises[name] = path.resolve(__dirname, exercises[name]);
+});
 
-var adventure = adventureMap(pkg)
+pkg.exercises = exercises;
+var adventure = adventureMap(pkg);
 
 // auto-execute if run from commandline
-if (!module.parent) adventure.execute(process.argv.slice(2))
+if (!module.parent) adventure.execute(process.argv.slice(2));
 
 // export for manual execution
-module.exports = adventure
+module.exports = adventure;
